79d32431c287ce96649f395faa286bf3db63f3b8
wahid9000 <wahidchowdhury9000@gmail.com>
2023-05-11 22:01:20 +0600

    api created for client side to book specific service

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 
@@ -34,7 +34,16 @@ async function run() {
         const cursor = servicesCollection.find();
         const result = await cursor.toArray();
         res.send(result)
+    })
 
+    app.get('/services/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const options = {
+            projection: { title: 1, price: 1, service_id: 1 },
+          };
+        const result = await servicesCollection.findOne(query, options);
+        res.send(result);
     })
 
 
