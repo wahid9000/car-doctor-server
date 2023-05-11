625906c6ce04514ca5c24a13f933eb3d6db195e3
wahid9000 <wahidchowdhury9000@gmail.com>
2023-05-12 02:02:54 +0600

    Revert "booking page created, user can add a booking with his information, CREATE operation done"
    This reverts commit dd49a5436351fb08a499dadf8815d59827390d45.

--- index.js
+++ index.js
@@ -16,58 +16,46 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    }
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
 async function run() {
-    try {
-        // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
 
-        const servicesCollection = client.db("carDoctorDB").collection("services");
-        const bookingCollection = client.db("carDoctorDB").collection("bookings");
+    const servicesCollection = client.db("carDoctorDB").collection("services")
 
-        app.get('/services', async (req, res) => {
-            const cursor = servicesCollection.find();
-            const result = await cursor.toArray();
-            res.send(result)
-        })
+    app.get('/services', async(req, res) => {
+        const cursor = servicesCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+    })
 
-        app.get('/services/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) };
-            const options = {
-                projection: { title: 1, price: 1, service_id: 1 },
-            };
-            const result = await servicesCollection.findOne(query, options);
-            res.send(result);
-        })
-
-
-        //bookings
-
-        app.post('/bookings', async (req, res) => {
-            const booking = req.body;
-            const result = await bookingCollection.insertOne(booking);
-            res.send(result)
-        });
+    app.get('/services/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const options = {
+            projection: { title: 1, price: 1, service_id: 1 },
+          };
+        const result = await servicesCollection.findOne(query, options);
+        res.send(result);
+    })
 
 
 
 
-
-
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
-    }
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
 }
 run().catch(console.dir);
 
