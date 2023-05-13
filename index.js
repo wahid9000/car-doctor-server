a772e73ea3fb47e9422f8a726d4d369e34ae30ff
wahid9000 <wahidchowdhury9000@gmail.com>
2023-05-13 17:35:48 +0600

    JWT token verification done

--- index.js
+++ index.js
@@ -24,6 +24,22 @@ const client = new MongoClient(uri, {
     }
 });
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if(!authorization) {
+        return res.status(401).send({error: true, message: 'unauthorized access'})
+    }
+    const token = authorization.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+        if(error){
+            return res.status(401).send({error: true, message: "Unauthorized access"})
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -63,7 +79,14 @@ async function run() {
 
         //bookings
 
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings', verifyJWT,  async (req, res) => {
+            const decoded = req.decoded;
+            console.log('came back after verify', decoded);
+
+            if(decoded.email !== req.query.email){
+                return res.status(403).send({error: 1, message: "forbidden access"})
+            }
+
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email }
